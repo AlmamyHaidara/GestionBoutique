bd9789cf0fa0cbf8970fb48b3c9aeb5d6d6163b2
Almamy <almamyh27@gmail.com>
2023-04-18 03:55:24 +0000

    Command crud programming

--- Controler/Data/Models/Vente_Model.py
+++ Controler/Data/Models/Vente_Model.py
@@ -1,6 +1,8 @@
 import sys
+from datetime import datetime
 
 from Controler.Data.Models.Produit_Model import Produit
+from Controler.Data.Models.Quantiter_Model import Quantiter
 from Controler.Data.Models.User_Model import User
 
 sys.path.append('../')
@@ -13,7 +15,9 @@ conn = connect()
 
 class Vente(Model):
     produit = ForeignKeyField(Produit, backref='produits')
+    quantiter = ForeignKeyField(Quantiter, backref='quantiter')
     user = ForeignKeyField(User, backref='users')
+    debut_date = DateField(default=datetime.now)
 
     class Meta:
         database = conn
